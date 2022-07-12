0ca6e1388283d92549ad2d6b6416aee4179133ae
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-07-12 18:58:55 -0300

    register page core

---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.scss']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
