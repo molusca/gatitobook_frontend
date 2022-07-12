fc359ab66d690f8e6debadbb5cc62c129eaa6ebe
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-07-12 19:20:21 -0300

    register page reactive forms core

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,14 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { NovoUsuario } from './novo-usuario.type';
 @Component({
   templateUrl: './novo-usuario.component.html',
   styleUrls: ['./novo-usuario.component.scss']
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  public form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  public buildForm(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  cadastraNovoUsuario(): void {
+    const novoUsuario = this.form.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+    // if (!this.form.valid) {
+    //   alert('Preencha os campos corretamente');
+    //   return;
+    // }
+
+    // const novoUsuario = this.form.value;
+    // this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe({
+    //   next: (res) => {
+    //     console.log(res);
+    //     alert('Usuario cadastrado com sucesso');
+    //   }
+    // });
+  }
 }
---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { NovoUsuario } from './novo-usuario.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  private _url = environment.apiUrl + '/user';
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastraNovoUsuario(NovoUsuario: NovoUsuario): Observable<any> {
+    return this.httpClient.post(`${this._url}/signup`, NovoUsuario);
+  }
+
+}
