645c8e2676b013a474453b2296f78b595aaced0d
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-07-19 20:18:46 -0300

    service animais

---
+++ src/app/animais/animais.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { TokenService } from '../autenticacao/token.service';
+import { Animais } from './animal';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimaisService {
+
+  private _url = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) { }
+
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+
+    return this.http.get<Animais>(`${this._url}/${nomeDoUsuario}/photos`, { headers });
+  }
+
+}
